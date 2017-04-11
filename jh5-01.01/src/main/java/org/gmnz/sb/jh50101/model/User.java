03aa49beb582bcc31c4fb0f09b9aa99b5e17db34
Simone <s3lvatico@gmail.com>
2017-04-11 17:24:56 +0200

    prima applicazione funzionante

--- jh5-01.01/src/main/java/org/gmnz/sb/jh50101/model/User.java
+++ jh5-01.01/src/main/java/org/gmnz/sb/jh50101/model/User.java
@@ -3,11 +3,15 @@ package org.gmnz.sb.jh50101.model;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
 
 
 @ManagedBean
 @SessionScoped
-public class User {
+public class User implements Serializable {
+
+
+	private static final long serialVersionUID = -1488800986294740922L;
 
 	private String name;
 	private String password;
@@ -34,5 +38,16 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+
+
+	@Override
+	public String toString() {
+		final StringBuffer sb = new StringBuffer("User{");
+		sb.append("name='").append(name).append('\'');
+		sb.append(", password='").append(password).append('\'');
+		sb.append('}');
+		return sb.toString();
 	}
 }
