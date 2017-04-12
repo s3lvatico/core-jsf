cbbe533901e6dcfa1db73c094e48a55963411189
s3lvatico <s3lvatico@gmail.com>
2017-04-12 13:23:14 +0200

    Pulizia e riorganizzazione generale

--- book examples/javaee/ch13/binary2/src/java/com/corejsf/BinaryData.java
+++
@@ -1,9 +0,0 @@
-package com.corejsf;
-
-import java.io.IOException;
-import java.io.OutputStream;
-
-public interface BinaryData {
-   String getContentType();
-   void write(OutputStream out) throws IOException;
-}
