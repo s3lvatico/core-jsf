edd430d092697558b53f9a979593a21de2638c4d
s3lvatico <s3lvatico@gmail.com>
2017-04-14 17:33:04 +0200

    reset totale

--- support/code/Core.JSF/tomcat/ch13/clientside-validator/src/java/com/corejsf/CreditCard.java
+++
@@ -1,9 +0,0 @@
-package com.corejsf;
-
-import java.io.Serializable;
-
-public class CreditCard implements Serializable {
-   public CreditCard(String number) { this.number = number; }
-   public String toString() { return number; }
-   private String number;
-}
