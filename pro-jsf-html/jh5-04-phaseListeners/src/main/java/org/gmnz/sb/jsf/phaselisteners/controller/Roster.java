69e3d6a4c4d83350dea1af533abef7681bda71bc
S3LvAtiCo <s3lvatico@gmail.com>
2017-05-01 16:54:43 +0200

    fixare la combobox

--- pro-jsf-html/jh5-04-phaseListeners/src/main/java/org/gmnz/sb/jsf/phaselisteners/controller/Roster.java
+++ pro-jsf-html/jh5-04-phaseListeners/src/main/java/org/gmnz/sb/jsf/phaselisteners/controller/Roster.java
@@ -1,6 +1,7 @@
 package org.gmnz.sb.jsf.phaselisteners.controller;
 
 import org.gmnz.sb.jsf.phaselisteners.model.Address;
+import org.gmnz.sb.jsf.phaselisteners.model.Profession;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
@@ -13,6 +14,11 @@ public class Roster implements Serializable {
 
     private static final long serialVersionUID = 4565522402435506436L;
 
+    private String name;
+    private Address address;
+    private Profession profession;
+    private String email = "--none set--";
+
     public Address getAddress() {
         return address;
     }
@@ -21,7 +27,29 @@ public class Roster implements Serializable {
         this.address = address;
     }
 
-    private Address address;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Profession getProfession() {
+        return profession;
+    }
+
+    public void setProfession(Profession profession) {
+        this.profession = profession;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String register() {
         System.out.println("register() method invoked");
