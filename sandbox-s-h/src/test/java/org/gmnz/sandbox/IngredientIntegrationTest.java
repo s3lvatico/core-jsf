ccea870e6f91e71d95f3a7f164b7e9b45f966299
S3LvAtiCo <s3lvatico@gmail.com>
2017-11-08 20:58:13 +0100

    IngredientIntegrationTest.java : scritto un test

--- sandbox-s-h/src/test/java/org/gmnz/sandbox/IngredientIntegrationTest.java
+++ sandbox-s-h/src/test/java/org/gmnz/sandbox/IngredientIntegrationTest.java
@@ -1,10 +1,13 @@
 package org.gmnz.sandbox;
 
+import org.gmnz.sandbox.domain.Ingredient;
 import org.gmnz.sandbox.service.IngredientService;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 public class IngredientIntegrationTest {
 
@@ -13,6 +16,15 @@ public class IngredientIntegrationTest {
 	@BeforeClass
 	public static void beforeClass() {
 		ctx = new ClassPathXmlApplicationContext("/spring-context.xml");
+	}
+
+	@Test
+	public void getAll() {
+		IngredientService svc = ctx.getBean(IngredientService.class);
+		List<Ingredient> ingredients = svc.getAllIngredients();
+		for (Ingredient i : ingredients) {
+			System.out.println(i);
+		}
 	}
 
 	@Test
